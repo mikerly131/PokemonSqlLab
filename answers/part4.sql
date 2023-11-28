d520093ffb8b668701bf6cf3636ccdda970df5a9
Mike <mike@ZipcodersMBP83.fios-router.home>
2023-11-27 21:55:28 -0500

    just need to sort now

--- answers/part4.sql
+++ answers/part4.sql
@@ -1,11 +1,15 @@
 -- Write a query that returns a bunch of stuff and sort it by an arbitrary decision
 -- Pokemon Name, Trainer Name, Pokemon Current Level, Pokemon Primary Type, Pokemon Secondary Type
-SELECT p.name, t.trainername, pt.pokelevel, p.primary_type, p.secondary_type
+-- I believe this is working, just not sorted.
+SELECT p.name, t.trainername, pt.pokelevel, tp.name, typ.name
 FROM pokemons as p
 JOIN pokemon_trainer as pt
 ON p.id = pt.pokemon_id
 JOIN trainers as t
-ON pt.trainerID = t.trainerID;
+ON pt.trainerID = t.trainerID
+JOIN types as tp
+ON p.primary_type = tp.id
+LEFT JOIN types as typ
+ON p.secondary_type = typ.id;
 
--- Need to figure out how to JOIN types as tp
 -- Need to sort by trainer with highest avg level of pokemon, and total power?
